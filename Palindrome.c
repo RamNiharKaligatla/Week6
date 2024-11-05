df3af5aa9c627524ab36539fe1f072ce1756e94f
Ram Nihar Kaligatla <bhavyagunaram@gmail.com>
2024-11-06 01:12:19 +0530

    Update Palindrome.c

--- Palindrome.c
+++ Palindrome.c
@@ -6,14 +6,22 @@ int main()
 	int i;
 	printf("Enter string: ");
 	scanf("%s",a);
-	int l=strlen(a);
-	if(a[i]==a[l-i-1])
+	int l=strlen(a),c=0;
+	for(i=0;a[i]!='\0';i++)
+	{
+		if(a[i]==a[l-i-1])
+		{
+			c++;
+		}
+		else
+		{
+			printf("It is not a palindrome");
+			break;
+		}
+	}
+	if(c==l)
 	{
 		printf("It is a palindrome");
-	}
-	else
-	{
-		printf("Not a palindrome");
 	}
 	return 0;
 }
