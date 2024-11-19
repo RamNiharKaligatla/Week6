cc721846c313ba367a7385abd157cdaf36d50fbc
Ram Nihar Kaligatla <bhavyagunaram@gmail.com>
2024-11-19 20:05:57 +0530

    Create

---
+++ NoOfWords.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <string.h>
+int main()
+{
+	char a[100];
+	printf("Enter string: ");
+	fgets(a,100,stdin);
+	int i,c=1;
+	for(i=0;a[i]!='\0';i++)
+	{
+		if(a[i]==' ')
+		{
+			c++;
+		}
+	}
+	printf("%d",c);
+	return 0;
+}
