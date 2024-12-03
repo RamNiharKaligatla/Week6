52fdd2f40449350a1db08bb39aa56e0099502312
Ram Nihar Kaligatla <bhavyagunaram@gmail.com>
2024-12-04 00:11:39 +0530

    Update Vowels.c

--- Vowels.c
+++ Vowels.c
@@ -1,16 +1,20 @@
 #include <stdio.h>
+
 int main()
 {
 	char a[100];
 	int i;
+
 	printf("Enter string: ");
-	scanf("%s",a);
-	for(i=0;a[i]!='\0';i++)
+	scanf("%s", a);
+
+	for (i = 0; a[i] != '\0'; i++)
 	{
-		if(a[i]=='a'||a[i]=='e'||a[i]=='i'||a[i]=='o'||a[i]=='u'||a[i]=='A'||a[i]=='E'||a[i]=='I'||a[i]=='O'||a[i]=='U')
+		if (a[i] == 'a' || a[i] == 'e' || a[i] == 'i' || a[i] == 'o' || a[i] == 'u' || a[i] == 'A' || a[i] == 'E' || a[i] == 'I' || a[i] == 'O' || a[i] == 'U')
 		{
-			printf("%c",a[i]);
+			printf("%c", a[i]);
 		}
 	}
+
 	return 0;
 }
