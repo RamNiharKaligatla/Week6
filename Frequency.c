d1fec28929a091741a1ef5559c1ec13f4906f612
Ram Nihar Kaligatla <bhavyagunaram@gmail.com>
2024-12-04 00:17:01 +0530

    Update Frequency.c

--- Frequency.c
+++ Frequency.c
@@ -1,30 +1,37 @@
 #include <stdio.h>
+
 int main()
 {
 	char a[100];
+
 	printf("Enter string: ");
-	scanf("%s",a);
-	int f[100],i,j;
-	for(i=0;a[i]!='\0';i++)
+	scanf("%s", a);
+
+	int f[100], i, j;
+
+	for (i = 0; a[i] != '\0'; i++)
 	{
-		f[i]=0;
+		f[i] = 0;
 	}
-	for(i=0;a[i]!='\0';i++)
+
+	for (i = 0; a[i] != '\0'; i++)
 	{
-		int c=1;
-		if(f[i]==1)
-			{
-				continue;
-			}
-		for(j=i+1;a[j]!='\0';j++)
+		int c = 1;
+		if (f[i] == 1)
 		{
-			if(a[i]==a[j])
+			continue;
+		}
+
+		for (j = i + 1; a[j] != '\0'; j++)
+		{
+			if (a[i] == a[j])
 			{
-				f[j]=1;
+				f[j] = 1;
 				c++;
 			}
 		}
-		printf("%c count: %d\n",a[i],c);
+		printf("%c count: %d\n", a[i], c);
 	}
+
 	return 0;
 }
