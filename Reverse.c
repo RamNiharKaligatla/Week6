5024d847dc9ddfd612eb5cf85988170433ad0794
Ram Nihar Kaligatla <bhavyagunaram@gmail.com>
2024-12-04 00:12:20 +0530

    Update Reverse.c

--- Reverse.c
+++ Reverse.c
@@ -1,18 +1,24 @@
 #include <stdio.h>
+
 int main()
 {
-	char a[100],b[100];
-	int i,j;
+	char a[100], b[100];
+	int i, j;
+
 	printf("Enter string: ");
-	scanf("%s",a);
-	for(j=0;a[j]!='\0';j++)
+	scanf("%s", a);
+
+	for (j = 0; a[j] != '\0'; j++)
 	{
 		;
 	}
-	for(i=0;i<j;i++)
+
+	for (i = 0; i < j; i++)
 	{
-		b[i]=a[j-i-1];
+		b[i] = a[j - i - 1];
 	}
-	printf("New string is: %s",b);
+
+	printf("New string is: %s", b);
+
 	return 0;
 }
